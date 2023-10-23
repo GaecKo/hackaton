8bf99ec1e62003d108887746b20f558d025ab405
aylin <aylindeoy@gmail.com>
2023-10-23 12:04:37 +0200

    3.2 code fini manque l'analyse des résultats

--- Hackaton_1/3.2.py
+++ Hackaton_1/3.2.py
@@ -1,9 +1,7 @@
-#%matplotlib inline
-import matplotlib.pyplot as plt
 import pandas as pd
-from statsmodels.tsa.api import acf, graphics, pacf
+from statsmodels.tsa.api import acf, graphics
 from statsmodels.tsa.ar_model import AutoReg, ar_select_order
-
+from sklearn.metrics import mean_absolute_error as mae 
 
 #1. split the dataset into training set and validation set
 
@@ -23,9 +21,32 @@ validation_set = v_set['Caen']
 #2. estimate this model with statsmodel on the training set
 
 model = AutoReg(training_set, lags=10)
-res = model.fit()
-print(res.summary())
+result = model.fit()
+
+print(result.summary())
 
 #3. judge quality
 
 #4. compute the Mean Absolute error (MAE) on the training set
+
+actual_data = training_set
+calculated_data = result.predict()
+
+# ===== Check for NaN values ===== #
+
+actual_data_nan = actual_data.isnull().any()
+calc_data_nan = calculated_data.isnull().any()
+
+# ===== Remove the missing values and make the sets the same size ===== #
+
+if actual_data_nan | calc_data_nan:
+    missing_rows = actual_data.isnull() | calculated_data.isnull()
+    actual_data = actual_data[~missing_rows]
+    calculated_data = calculated_data[~missing_rows]
+
+# ===== Calculate the MAE ===== #
+
+
+MAE = mae(actual_data, calculated_data)
+
+print("The Mean Absolute Error between predicted and real consumptions on the training set is: {} W/m²".format(MAE))
