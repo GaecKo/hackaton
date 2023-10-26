b979f3ff1bc77d8ed0bea87a1f73d1e65277e81e
aylin <aylindeoy@gmail.com>
2023-10-26 16:02:17 +0200

    3.2 finished

--- Hackaton_1/3.2.py
+++ Hackaton_1/3.2.py
@@ -1,6 +1,6 @@
 import pandas as pd
-from statsmodels.tsa.api import acf, graphics
-from statsmodels.tsa.ar_model import AutoReg, ar_select_order
+from matplotlib import pyplot as plt
+from statsmodels.tsa.ar_model import AutoReg
 from sklearn.metrics import mean_absolute_error as mae 
 
 #1. split the dataset into training set and validation set
@@ -13,39 +13,35 @@ df['YEAR'] = pd.to_datetime(df['DATE'], format='%Y%m%d').dt.year
 # ===== Divide datasets ===== #
 
 t_set = df[(df['YEAR'] >= 1977) & (df['YEAR'] <= 2010)]   #1977-2010
-training_set = t_set['Caen']
+training_set = t_set[['YEAR', 'Caen']]
 v_set = df[(df['YEAR'] >= 2011) & (df['YEAR'] <= 2019)] #2011-2019
-validation_set = v_set['Caen']
-
+validation_set = v_set[['YEAR', 'Caen']]
 
 #2. estimate this model with statsmodel on the training set
 
-model = AutoReg(training_set, lags=10)
+model = AutoReg(training_set['Caen'], lags=10)
 result = model.fit()
 
 print(result.summary())
 
 #3. judge quality
 
-#4. compute the Mean Absolute error (MAE) on the training set
-
-actual_data = training_set
+actual_data = training_set['Caen']
 calculated_data = result.predict()
 
-# ===== Check for NaN values ===== #
 
-actual_data_nan = actual_data.isnull().any()
-calc_data_nan = calculated_data.isnull().any()
+plt.plot(training_set['YEAR'], actual_data/10, label="Measured Temp.", linestyle="-", color="blue")
+plt.plot(training_set['YEAR'], calculated_data.values/10, label="Predicted Temp.", linestyle="--", color="red")
 
-# ===== Remove the missing values and make the sets the same size ===== #
+plt.title("Measured temperature and predicted temperature by AutoReg model")
+plt.xlabel("Date")
+plt.ylabel("Temperature (celsius)")
+plt.legend()
+plt.show()
 
-if actual_data_nan | calc_data_nan:
-    missing_rows = actual_data.isnull() | calculated_data.isnull()
-    actual_data = actual_data[~missing_rows]
-    calculated_data = calculated_data[~missing_rows]
+#4. compute the Mean Absolute error (MAE) on the training set
 
 # ===== Calculate the MAE ===== #
-
 
 MAE = mae(actual_data, calculated_data)
 
