d238d807c3032ffcfffeeecae906c9606a0af5e5
aylin <aylindeoy@gmail.com>
2023-10-26 17:04:28 +0200

    3.3 code finished reste review

--- Hackaton_1/3.2.py
+++ Hackaton_1/3.2.py
@@ -46,3 +46,42 @@ plt.show()
 MAE = mae(actual_data, calculated_data)
 
 print("The Mean Absolute Error between predicted and real consumptions on the training set is: {} W/m²".format(MAE))
+
+#3.3
+
+from sklearn.metrics import r2_score
+
+#Compare on a graph, the forecast to  real consumptions on the given period
+model2 = AutoReg(validation_set['Caen'], lags=10)
+result2 = model.fit()
+
+actual_data1 = validation_set['Caen']
+calculated_data1 = result2.predict()
+
+
+plt.plot(validation_set['YEAR'], actual_data1/10, label="Measured Temp.", linestyle="-", color="blue")
+plt.plot(validation_set['YEAR'], calculated_data1.values/10, label="Predicted Temp.", linestyle="--", color="red")
+
+plt.title("Measured temperature and predicted temperature by AutoReg model")
+plt.xlabel("Date")
+plt.ylabel("Temperature (celsius)")
+plt.legend()
+plt.show()
+
+#Plot the errors of prediction. Are they acceptable?
+
+prediction_error = actual_data1 - calculated_data1
+plt.figure(figsize=(12,12))
+plt.plot(validation_set['YEAR'], prediction_error, label="Prediction errors", color="green")
+plt.title("Prediction Errors on the validation set")
+plt.xlabel("Date")
+plt.ylabel("Error")
+plt.legend()
+plt.show()
+
+#Compute the MAE on the test set and the R². Is the forecast reliable?
+
+mae2 = mae(actual_data1, calculated_data1)
+r_squared_val = r2_score(actual_data1, calculated_data1)
+
+print("The MAE and the R² values are respectively: {} and {}".format(mae2, r_squared_val))
